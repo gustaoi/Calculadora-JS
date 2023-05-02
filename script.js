14aec632474ab1d16e393182b50f424a224860bb
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-05-01 23:07:17 -0300

    iniciate js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //Script da Calculadora
+
+let runningtotal = 0;
+let buffer = "0";
+let previosOperator;
+
+const screen = document.querySelector('.screen');
+
+function button(value) {
+    if (isNaN(value)) {
+        handleSymbol(value)
+    }
+}
