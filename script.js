e8b32a028019e6adbcc550b009cd013c01887f66
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-05-02 14:13:39 -0300

    Caculator finished

--- script.js
+++ script.js
@@ -2,12 +2,88 @@
 
 let runningtotal = 0;
 let buffer = "0";
-let previosOperator;
+let previousOperator;
 
 const screen = document.querySelector('.screen');
 
-function button(value) {
+function buttonClick(value) {
     if (isNaN(value)) {
-        handleSymbol(value)
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case 'C':
+            buffer = "0";
+            runningtotal = 0;
+            break;
+        case "=":
+            if (previousOperator === null) {
+                return
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = runningtotal;
+            runningtotal = 0;
+            break;
+        case '←':
+            if(buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1)
+            }
+            break;
+        case '+':
+        case '-':
+        case 'x':
+        case '÷':
+            handleMath(symbol);
+            break;
     }
 }
+
+function handleMath(symbol) {
+    if (buffer === '0') {
+        return;
+    }
+    
+        const intBuffer = parseInt(buffer);
+
+        if (runningtotal === 0) {
+            runningtotal = intBuffer;
+        } else {
+            flushOperation(intBuffer);
+        }
+        previousOperator = symbol;
+        buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningtotal += intBuffer;
+    }else if(previousOperator === '-') {
+        runningtotal -= intBuffer;
+    }else if(previousOperator === 'x') {
+        runningtotal *= intBuffer;
+    }else if(previousOperator === '÷') {
+        runningtotal /= intBuffer;
+    }
+}
+
+function handleNumber(numberString) {
+    if(buffer === '0') {
+        buffer = numberString;
+    }else {
+        buffer +=numberString
+    }
+}
+
+function init() {
+    document.querySelector('.calc-buttons').addEventListener('click', function(event){buttonClick(event.target.innerText)});
+}
+
+init();
